e0c69f882f50169c8ae7e5f9bcd6ee8b4e4066bd
Kazoki-ken <saidxongoogl@gmail.com>
2026-06-05 10:30:51 +0500

    fix: ignore react-native-worklets in autolinking for EasyNihongo

---
+++ apps/mobile/react-native.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  dependencies: {
+    'react-native-worklets': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    'react-native-worklets-core': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
+};
